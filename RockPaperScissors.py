1699a1dae57c9692dee05869cdee224116cdf4d5
Maxmus9487 <146067608+Maxmus9487@users.noreply.github.com>
2023-10-08 10:29:42 +0200

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,34 +1,57 @@
+import sys
 import random
-
-choices = ("rock", "paper", "sicssors")
-player = None
-computer = random.choice(choices)
-running = True
-while True:
-    player = None
-    computer = random.choice(choices)
-    while player not in choices:
-        player = input(("Enter... \n rock,\n paper,\n scissors: \n\n"))
+from enum import Enum
 
 
-        print("")
-        print(f"Player:{player}")
-        print(f"Python:{computer}")
-        print("")
-
-        if player == computer:
-            print("⧓ Tie game")
-        elif player == "rock" and computer == "sicssors":
-            print("🥳🎉 You win")
-        elif player == "paper" and computer == "rock":
-            print("🥳🎉 You win")
-        elif player == "sicssors" and computer == "paper":
-            print("🥳🎉 You win")
-        else:
-            print("😞❌ You lose")
-
-    replay = input("Play again? (Yes/No)").lower()
-    if not replay =="Yes":
-        running = False
+def play_rps():
     
-    print("Thanks for playing!")
+    class RPS(Enum):
+        Rock = 1
+        Paper = 2
+        Scissors = 3
+
+
+    playerchoice= input("Enter... \n 1 for rock,\n 2 for paper,\n 3 for scissors: \n\n")
+
+    if playerchoice not in ["1","2", "3"]:
+        print("you must enter 1, 2, or 3.")
+        return play_rps()
+    
+    player = int(playerchoice)
+
+    computerchoice = random.choice("123")
+
+    computer = int(computerchoice)
+
+    print("")
+    print("you chose " + str(RPS(player)).replace('RPS.', '') + ".")
+    print("python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+    print("")
+
+    if player == 1 and computer == 3:
+        print("🥳🎉 you win")
+    elif player == 2 and computer == 1:
+        print("🥳🎉 you win")
+    elif player == 3 and computer == 2:
+        print("🥳🎉 you win")
+    elif player == computer:
+        print("⧓ tie game")
+    else:
+        print("😞❌ you lose")
+
+    print("\nPlay again?")
+
+    while True:
+        playagain = input("\nY for Yes or \n Q for Quit\n")
+        if playagain.lower() not in ["y", "q"]:
+            continue
+        else:
+            break
+
+    if playagain.lower() == "y":
+        return play_rps()
+    else:
+        print("\n Thank you for playing\n")
+        sys.exit(" Bye!")
+
+play_rps()
